913ebd5b0800bc9fe4006260aa44573b1ecb7882
rtsh13 <ruchitsingh13@gmail.com>
2024-05-08 00:51:20 +0530

    insert and verify membership

---
+++ pkg/bloomFilter/ops.go
@@ -0,0 +1,29 @@
+package bloomfilter
+
+func (f *Filter) InsertAt(v []byte) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	segment1, segment2 := murmum3_128(v)
+	indexes := f.hashToBitsetIdx([]uint64{segment1, segment2, fnv1a(v)}...)
+
+	for _, idx := range indexes {
+		f.bSet.Set(idx)
+	}
+}
+
+func (f *Filter) MemberOf(v []byte) bool {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	segment1, segment2 := murmum3_128(v)
+	indexes := f.hashToBitsetIdx([]uint64{segment1, segment2, fnv1a(v)}...)
+
+	for _, idx := range indexes {
+		if f.bSet.Test(idx) {
+			return true
+		}
+	}
+
+	return false
+}
