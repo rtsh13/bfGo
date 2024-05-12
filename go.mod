98178986df3d5a1b35965eb1ce3faf6e2a8a9172
rtsh13 <ruchitsingh13@gmail.com>
2024-05-12 12:55:45 +0530

    code-cov: 100%

--- go.mod
+++ go.mod
@@ -5,5 +5,12 @@ go 1.22.2
 require (
 	github.com/bits-and-blooms/bitset v1.13.0
 	github.com/google/uuid v1.6.0
+	github.com/stretchr/testify v1.9.0
 	github.com/twmb/murmur3 v1.1.8
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/bloom/bloom_test.go
@@ -0,0 +1,65 @@
+package bloom
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Insert(t *testing.T) {
+	t.Parallel()
+
+	bloomFt := New(WithSize(10))
+
+	preSetPopCnt := bloomFt.PopCnt()
+	bloomFt.Insert([]byte("hi"))
+	postSetPopCnt := bloomFt.PopCnt()
+
+	assert.GreaterOrEqualf(t, postSetPopCnt, preSetPopCnt, "filter->insert")
+}
+
+func Test_MemberOf(t *testing.T) {
+	t.Parallel()
+
+	bloomFt := New(WithSize(10))
+
+	testCases := []struct {
+		key       int
+		input     []byte
+		expResult bool
+	}{
+		{1, []byte("hi"), true},
+		{2, []byte("bye"), false},
+		{3, []byte(uuid.NewString()), true},
+		{4, []byte(""), false},
+	}
+
+	for _, tt := range testCases {
+		if tt.key != 2 && tt.key != 4 {
+			bloomFt.Insert(tt.input)
+			result := bloomFt.MemberOf(tt.input)
+			assert.Equalf(t, tt.expResult, result, "filter->memberOf")
+			continue
+		}
+
+		result := bloomFt.MemberOf(tt.input)
+		assert.Equalf(t, tt.expResult, result, "filter->memberOf")
+	}
+}
+
+func Test_Flush(t *testing.T) {
+	t.Parallel()
+
+	bloomFt := New(WithSize(10))
+
+	preSeed := bloomFt.PopCnt()
+	bloomFt.Insert([]byte("hi"))
+	postSeed := bloomFt.PopCnt()
+	bloomFt.Flush()
+	postFlush := bloomFt.PopCnt()
+
+	assert.GreaterOrEqualf(t, postSeed, preSeed, "filter->flush")
+	assert.Less(t, postFlush, postSeed, "filter->flush")
+	assert.Equalf(t, preSeed, postFlush, "filter->flush")
+}
